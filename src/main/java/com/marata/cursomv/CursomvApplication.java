e66f83c5ec567dc62a903e023d03a265fba98862
Josenias Nascimento <siberiun@gmail.com>
2021-06-02 09:28:20 -0300

    Projeto criado

---
+++ src/main/java/com/marata/cursomv/CursomvApplication.java
@@ -0,0 +1,13 @@
+package com.marata.cursomv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marata/cursomv/CursomvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marata.cursomv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
