d493d4c072aa263cac38002fa7027569f9b4a70f
Josenias Nascimento <siberiun@gmail.com>
2021-06-02 13:30:03 -0300

    Criando operação de instanciação

--- src/main/java/com/marata/cursomv/CursomvApplication.java
+++ src/main/java/com/marata/cursomv/CursomvApplication.java
@@ -1,13 +1,32 @@
 package com.marata.cursomv;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomvApplication {
+import com.marata.cursomv.domain.Categoria;
+import com.marata.cursomv.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomvApplication implements CommandLineRunner {
+    
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
