dc57eea8008c3faac9b3bba3f09e1935e5e17f38
Josenias Nascimento <siberiun@gmail.com>
2021-06-02 12:18:39 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/marata/cursomv/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.marata.cursomv.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marata.cursomv.domain.Categoria;
+import com.marata.cursomv.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+    
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
