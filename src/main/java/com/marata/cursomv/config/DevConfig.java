58cc81b6eec2eac03d8233a7ac291e765ef25da7
Josenias Nascimento <siberiun@gmail.com>
2021-06-17 14:08:23 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/marata/cursomv/config/DevConfig.java
+++ src/main/java/com/marata/cursomv/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.marata.cursomv.services.DBService;
+import com.marata.cursomv.services.EmailService;
+import com.marata.cursomv.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,5 +30,10 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/marata/cursomv/config/TestConfig.java
+++ src/main/java/com/marata/cursomv/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.marata.cursomv.services.DBService;
 import com.marata.cursomv.services.EmailService;
 import com.marata.cursomv.services.MockEmailService;
+import com.marata.cursomv.services.SmtpEmailService;
 
 @Configuration
 @Profile("test")
@@ -26,6 +27,7 @@ public class TestConfig {
 	
 	@Bean
 	public EmailService emailService() {
-		return new MockEmailService();
+		//return new MockEmailService();
+		return new SmtpEmailService();
 	}
 }
