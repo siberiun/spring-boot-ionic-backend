b7d74a0f7033dfc016e70250be5d65e009a3d873
Josenias Nascimento <siberiun@gmail.com>
2021-06-19 17:44:52 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/marata/cursomv/services/ClienteService.java
+++ src/main/java/com/marata/cursomv/services/ClienteService.java
@@ -49,6 +49,9 @@ public class ClienteService {
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Cliente find(Integer id) {
 		// Recupera o cliente logado
@@ -126,6 +129,10 @@ public class ClienteService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multiPartFile);
+		
+		//Recortar a imagem
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String filename = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), filename, "image");
