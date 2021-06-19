82f51437160a84362929c91121343fa8ba309fd3
Josenias Nascimento <siberiun@gmail.com>
2021-06-19 15:07:38 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/marata/cursomv/domain/Cliente.java
+++ src/main/java/com/marata/cursomv/domain/Cliente.java
@@ -36,8 +36,6 @@ public class Cliente implements Serializable {
 
 	@JsonIgnore
 	private String senha;
-	
-	private String imageUrl;
 
 	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -172,14 +170,6 @@ public class Cliente implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
 	}
 
 }
